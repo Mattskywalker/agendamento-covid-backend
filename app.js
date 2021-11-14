c623115f3de37e27a70fa0e0901658702f2ec5df
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-11-14 18:42:31 -0300

    bodyparser

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require('express');
 
 const app = express();
 const userRoute = require('./routes/user');
-
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/users', userRoute);
 
