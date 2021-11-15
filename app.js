588702b77cfecd8c18e35c21a933b35c9e9509c3
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-11-15 20:48:02 -0300

    regra de cors e import atualizado

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 
 const app = express();
-const userRoute = require('./routes/user');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const userRoute = require('./src/routes/userRoute')
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Acces-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+        'Acces-Control-Allow-Header',
+        'Origin, X-Requrested-With, Content-Type, Accept, Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Origin', 'PUT, POST,PATCH, DELETE, GET');
+        return res.status(200).send({});
+    }
+    next();
+})
 
 app.use('/users', userRoute);
 
@@ -23,5 +35,6 @@ app.use((error, req,res,next) => {
         error: error.message,
     })
 });
+
 
 module.exports = app;
