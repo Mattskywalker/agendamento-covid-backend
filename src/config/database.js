458d72e661821ab4caa63ba88755aebab9f5aacc
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-11-15 20:45:59 -0300

    configuração do banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = { 
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'mattskywalker',
+    database: 'nodeApi',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
