0606d5bf56b0d298d41da30ca6717e077d58274d
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-11-15 20:42:09 -0300

    [organização] - Server movida para a src

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 const http = require('http');
-const app = require('./app')
+const app = require('../app')
+
+require('./database/index')
+
 const port = process.env.PORT || 8080;
 
 const server = http.createServer(app);
 
 server.listen(port);
+
+
 
