aaf55dc1d439bda4cc006f5312edd0ff8cdde5e5
Mateus J. Martins <mateus.lima@partners.mv.com.br>
2021-11-16 11:46:42 -0300

    metodo find all

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -2,11 +2,7 @@ const express = require('express');
 const UserController = require('../controllers/UserController');
 const router = express.Router();
 
-router.get('/',(req,res,next) => {
-    res.status(200).send({
-        msg: 'rota get usuarios'
-    })
-});
+router.get('/all', UserController.index)
 
 router.post('/',UserController.store);
 
